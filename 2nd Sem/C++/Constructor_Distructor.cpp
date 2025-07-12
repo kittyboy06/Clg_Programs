30cd1b8507064f5a6a0c7f4ade73b4a25b9bb695
Kittyboy06 <afsalahmed2006@gmail.com>
2025-07-12 20:23:17 +0530

    Class

---
+++ 2nd Sem/C++/Constructor_Distructor.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    char name[20];
+    int roll;
+    int marks;
+    int phno;
+
+public:
+    Student(const char* n, int r, int m, int p) {
+        strcpy(name, n);
+        roll = r;
+        marks = m;
+        phno = p;
+    }
+
+    ~Student() {
+        cout << "Destructor called for " << name << endl;
+    }
+
+    void displayData() {
+        cout << "Name: " << name
+             << ", Roll No: " << roll
+             << ", Marks: " << marks
+             << ", Phone No: " << phno << endl;
+    }
+};
+
+int main() {
+    Student s1("Alice", 101, 90, 1234567890);
+    Student s2("Bob", 102, 85, 9876543210);
+
+    s1.displayData();
+    s2.displayData();
+
+    return 0;
+}
