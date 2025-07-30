6d1527a6a4457b71913e744b908823db505e2eb0
Kittyboy06 <afsalahmed2006@gmail.com>
2025-07-30 09:42:50 +0530

    Lab_30/07

--- 2nd Sem/C++/area.cpp
+++ 2nd Sem/C++/area.cpp
@@ -1,32 +1,57 @@
 #include<iostream>
 using namespace std;
 
-class rectangle {
-private:
-    int length,breadth, area;
-public:
-    void getData()
-    {
-        cout << "Enter the values for  length and breadth: ";
-        cin >> length >> breadth;
-    }
+double areacircle(double rad)
+{
+    return 3.14 * rad * rad;
+}
 
-    void comp()
-    {
-        area = length * breadth;
-    }
+double arearectangle(double length, double breadth)
+{
+    return length * breadth;
+}
 
-    void display()
-    {
-        cout << "Area of rectangle: " << area << endl;
-    }
-};
+double areasquare(double side)
+{
+    return side * side;
+}
 
 int main()
 {
-    rectangle r;
-    r.getData();
-    r.comp();
-    r.display();
+    double radius, length, breadth, side;
+    int choice;
+
+    
+
+    while (true)
+    {
+        cout << "Choose the shape to calculate area:\n1. Circle\n2. Rectangle\n3. Square\n4. Exit\n";
+        cout << "Enter your choice (1-4): ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter radius of circle: ";
+            cin >> radius;
+            cout << "Area of Circle: " << areacircle(radius) << endl;
+            break;
+        case 2:
+            cout << "Enter length and breadth of rectangle: ";
+            cin >> length >> breadth;
+            cout << "Area of Rectangle: " << arearectangle(length, breadth) << endl;
+            break;
+        case 3:
+            cout << "Enter side of square: ";
+            cin >> side;
+            cout << "Area of Square: " << areasquare(side) << endl;
+            break;
+        case 4:
+            cout << "Exiting the program." << endl;
+            return 0;
+        default:
+            cout << "Invalid choice!" << endl;
+        }
+    }
+
     return 0;
 }
