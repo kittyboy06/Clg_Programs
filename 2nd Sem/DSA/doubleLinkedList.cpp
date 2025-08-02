af94a0814730c4bd212e45caa3e447d0f94780c3
Kittyboy06 <afsalahmed2006@gmail.com>
2025-08-02 11:40:11 +0530

    DSA

--- 2nd Sem/DSA/doubleLinkedList.cpp
+++ 2nd Sem/DSA/doubleLinkedList.cpp
@@ -5,4 +5,53 @@ struct doubleLinkedList
     int data;
     doubleLinkedList* next;
     doubleLinkedList* prev;
-};
+}*head, *tail, *temp, *newNode;
+
+void createNode(int value)
+{
+    newNode = (struct doubleLinkedList*)malloc(sizeof(struct doubleLinkedList));
+    newNode->data = value;
+    newNode->next = nullptr;
+    newNode->prev = nullptr;
+
+    if (head == nullptr) {
+        head = tail = newNode;
+    } else {
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+}
+
+void insertAtPosition(int value, int position) {
+    newNode = (struct doubleLinkedList*)malloc(sizeof(struct doubleLinkedList));
+    temp = head;
+    newNode->data = value;
+    newNode->next = nullptr;
+    newNode->prev = nullptr;
+
+    if (position == 1) 
+    {
+        newNode->next = head;
+        if (temp->prev == nullptr) 
+        {
+            head->prev = newNode;
+        }
+        head = newNode; 
+    }
+
+    temp = head;
+    for (int i = 0; i < position && temp != nullptr; i++) 
+    {
+        temp = temp->next;
+    }
+    
+    newNode->next = temp;
+    newNode->prev = temp;
+    temp->next = newNode;
+    if (newNode->next != nullptr) {
+        newNode->next->prev = newNode;
+    } else {
+        tail = newNode;
+    }
+}
---
+++ 2nd Sem/Ds/uni.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import skew, kurtosis
+print("Reading data from diabetes dataset...")
+path_1 = "C:/Users/DELL/Desktop/2nd Sem/Ds/diabetes.csv"
+path_2 = "C:/Users/DELL/Desktop/2nd Sem/Ds/data-03.csv"
