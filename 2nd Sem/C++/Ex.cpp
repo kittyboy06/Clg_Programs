04be6680bd3b031dac27548058a5657f57df40a3
Kittyboy06 <afsalahmed2006@gmail.com>
2025-09-01 15:16:42 +0530

    Excep

---
+++ 2nd Sem/C++/Ex.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Entert the values of a and b\n";
+    cin>>a>>b;
+    int x=a/b;
+    try
+    {
+        if(x!=0)
+        {
+            cout<<"Result(a/x)="<<a/x<<"\n";
+
+        }
+        else
+        {
+            throw(x);
+        }
+    }
+    catch(int i)
+    {
+        cout<<"Exception caught : x="<<x<<"\n";
+
+    }
+    cout<<"END";
+    return 0;
+
+
+}
