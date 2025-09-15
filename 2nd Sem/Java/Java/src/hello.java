f9e160447f21cc34b3c4da32591ea0c70bfe5733
Kittyboy06 <afsalahmed2006@gmail.com>
2025-09-15 14:16:07 +0530

    javaremove

--- 2nd Sem/Java/Java/src/hello.java
+++
@@ -1,7 +0,0 @@
-import static java.lang.System.*;
-
-public class hello{
-    public static void main(String[] args) {
-        out.println("Hello, World!");
-    }
-}
---
+++ 2nd Sem/Java/h.java
@@ -0,0 +1,7 @@
+package 2nd Sem.Java;
+
+public class h {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
