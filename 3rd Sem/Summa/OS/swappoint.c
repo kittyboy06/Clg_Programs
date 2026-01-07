3310c15352f83090bdb4cdf30721d0aebfdfecf1
Kittyboy06 <afsalahmed2006@gmai.com>
2026-01-07 13:11:20 +0530

    4

---
+++ 3rd Sem/Summa/OS/swappoint.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() 
+{
+    int x, y;
+    printf("Enter two integers to swap: ");
+    scanf("%d %d", &x, &y);
+    
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swapping: x = %d, y = %d\n", x, y);
+    
+    getch();
+    return 0;
+}
