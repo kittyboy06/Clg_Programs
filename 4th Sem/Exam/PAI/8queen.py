6600d682ba6b3632839529e37ebc0fb87e6313df
kittyboy06 <afsalahmed2006@gmail.com>
2026-06-21 19:25:36 +0530

    Done

--- 4th Sem/Exam/PAI/8queen.py
+++ 4th Sem/Exam/PAI/8queen.py
@@ -1,10 +1,10 @@
 import random
 
 def solve(q=[], col=0):
-    if col == 8:
+    if col == 4:
         print_board(q)
         return True
-    rows = list(range(8))
+    rows = list(range(4))
     random.shuffle(rows)  # Shuffle row order for randomness
     for row in rows:
         if all(row != r and abs(row - r) != col - c for c, r in enumerate(q)):
@@ -14,7 +14,7 @@ def solve(q=[], col=0):
 
 def print_board(q):
     for row in q:
-        print(". " * row + "Q " + ". " * (7 - row))
+        print(". " * row + "Q " + ". " * (3 - row))
     print()
 
 solve()
